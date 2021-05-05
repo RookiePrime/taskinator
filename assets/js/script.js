be0a9af4fd2ba1fb8b8036b348a8ed2e0297a01e
Kiefer Levine <kiefer_8@hotmail.com>
2021-05-05 15:20:15 -0400

    Added partial form submission support.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-const buttonE1 = document.querySelector("#save-task");
+const formE1 = document.querySelector("#task-form");
 const tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-const createTaskHandler = () => {
+const createTaskHandler = function(event) {
+    event.preventDefault();
+
     const listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task";
     tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler());
+formE1.addEventListener("submit", createTaskHandler(event));
